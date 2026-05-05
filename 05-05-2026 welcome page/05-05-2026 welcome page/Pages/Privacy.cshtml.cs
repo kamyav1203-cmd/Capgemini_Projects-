0fbebacbf165e2d8acd61585086af50acef0841b
kamyavyas12 <kamyav1203@gmail.com>
2026-05-05 11:29:44 +0530

    new files added

---
+++ 05-05-2026 welcome page/05-05-2026 welcome page/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _05_05_2026_welcome_page.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
